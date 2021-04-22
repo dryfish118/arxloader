9a99cd4e2c43128bf43685bab3d85a75bb5e0131
dryfish118 <dryfish118@163.com>
2021-04-22 21:55:16 +0800

    dialog

--- arxloader/dlg/arxlistDlg.cpp
+++ arxloader/dlg/arxlistDlg.cpp
@@ -26,10 +26,32 @@ void CarxlistDlg::DoDataExchange(CDataExchange* pDX)
 
 BEGIN_MESSAGE_MAP(CarxlistDlg, CAcUiDialog)
   ON_WM_DESTROY()
+  ON_NOTIFY(TVN_ITEMCHANGED, IDC_TREE_TESTCASE, &CarxlistDlg::OnTvnItemChangedTree)
+  ON_NOTIFY(NM_DBLCLK, IDC_TREE_TESTCASE, &CarxlistDlg::OnNMDblclkTree)
+  ON_BN_CLICKED(IDC_BUTTON_FILE, &CarxlistDlg::OnBnClickedButtonFile)
+  ON_BN_CLICKED(IDC_BUTTON_VIEW, &CarxlistDlg::OnBnClickedButtonView)
+  ON_BN_CLICKED(IDOK, &CarxlistDlg::OnBnClickedOk)
 END_MESSAGE_MAP()
 
 
 // CarxlistDlg 消息处理程序
+
+static CString documentsPath()
+{
+  LPITEMIDLIST pidl = nullptr;
+  if (SUCCEEDED(SHGetSpecialFolderLocation(NULL, CSIDL_MYDOCUMENTS, &pidl)))
+  {
+    wchar_t szPath[MAX_PATH] = { 0 };
+    if (SHGetPathFromIDList(pidl, szPath))
+    {
+      CoTaskMemFree(pidl);
+
+      return szPath;
+    }
+  }
+
+  return L"";
+}
 
 BOOL CarxlistDlg::OnInitDialog()
 {
@@ -39,11 +61,28 @@ BOOL CarxlistDlg::OnInitDialog()
 
   StretchControlXY(m_treeArx.GetDlgCtrlID(), 100, 100);
   MoveControlXY(m_treeArx.GetDlgCtrlID(), 0, 0);
+  StretchControlX(IDC_STATIC_LOG, 100);
+  MoveControlXY(IDC_STATIC_LOG, 0, 100);
+  MoveControlXY(IDC_BUTTON_FILE, 100, 100);
+  MoveControlXY(IDC_BUTTON_VIEW, 100, 100);
   MoveControlXY(IDOK, 100, 100);
 
-  m_treeArx.ModifyStyle(0,TVS_HASBUTTONS | TVS_HASLINES | 
-    TVS_LINESATROOT | TVS_DISABLEDRAGDROP | TVS_CHECKBOXES |
-    TVS_TRACKSELECT | /*TVS_SINGLEEXPAND | */TVS_FULLROWSELECT);
+  m_treeArx.ModifyStyle(0, TVS_HASBUTTONS | TVS_HASLINES |
+    TVS_LINESATROOT | TVS_DISABLEDRAGDROP | TVS_CHECKBOXES);
+  initTree();
+
+  SetDlgItemText(IDC_STATIC_LOG, documentsPath() + L"\\resuts.log");
+
+	return TRUE;  // 除非将焦点设置到控件，否则返回 TRUE
+}
+
+void CarxlistDlg::OnDestroy()
+{
+  CAcUiDialog::OnDestroy();
+}
+
+void CarxlistDlg::initTree()
+{
   HTREEITEM hRoot = m_treeArx.InsertItem(L"Dlg test cases");
   m_treeArx.InsertItem(L"Test CAcUiDialog", hRoot);
   m_treeArx.Expand(hRoot, TVE_EXPAND);
@@ -55,11 +94,51 @@ BOOL CarxlistDlg::OnInitDialog()
   m_treeArx.InsertItem(L"Read the field info in an object", hRoot);
   m_treeArx.Expand(hRoot, TVE_EXPAND);
   m_treeArx.SetCheck(hRoot);
-
-	return TRUE;  // 除非将焦点设置到控件，否则返回 TRUE
 }
 
-void CarxlistDlg::OnDestroy()
+void CarxlistDlg::OnTvnItemChangedTree(NMHDR *pNMHDR, LRESULT *pResult)
 {
-  CAcUiDialog::OnDestroy();
+  NMTVITEMCHANGE *pNMTVItemChange = reinterpret_cast<NMTVITEMCHANGE*>(pNMHDR);
+  HTREEITEM hItem = pNMTVItemChange->hItem;
+  if (m_treeArx.GetParentItem(hItem) == nullptr)
+  {
+    BOOL ck = m_treeArx.GetCheck(hItem);
+    HTREEITEM hChild = m_treeArx.GetChildItem(hItem);
+    while (hChild)
+    {
+      m_treeArx.SetCheck(hChild, ck);
+      hChild = m_treeArx.GetNextItem(hChild, TVGN_NEXT);
+    }
+  }
+
+  *pResult = 0;
+}
+
+void CarxlistDlg::OnNMDblclkTree(NMHDR *pNMHDR, LRESULT *pResult)
+{
+  HTREEITEM hItem = m_treeArx.GetSelectedItem();
+  if (hItem != nullptr)
+  {
+    m_treeArx.SetCheck(hItem, !m_treeArx.GetCheck(hItem));
+  }
+
+  *pResult = 1;
+}
+
+
+void CarxlistDlg::OnBnClickedButtonFile()
+{
+}
+
+
+void CarxlistDlg::OnBnClickedButtonView()
+{
+  CString fileName;
+  GetDlgItemText(IDC_STATIC_LOG, fileName);
+  ShellExecute(NULL, NULL, fileName, NULL, NULL, SW_SHOW);
+}
+
+
+void CarxlistDlg::OnBnClickedOk()
+{
 }
--- arxloader/dlg/arxlistDlg.h
+++ arxloader/dlg/arxlistDlg.h
@@ -23,12 +23,19 @@ public:
 
 // 实现
 protected:
+  void initTree();
 
 	// 生成的消息映射函数
 	virtual BOOL OnInitDialog();
   afx_msg void OnDestroy();
+  afx_msg void OnTvnItemChangedTree(NMHDR *pNMHDR, LRESULT *pResult);
+  afx_msg void OnNMDblclkTree(NMHDR *pNMHDR, LRESULT *pResult);
 	DECLARE_MESSAGE_MAP()
 
 private:
   CTreeCtrl m_treeArx;
+public:
+  afx_msg void OnBnClickedButtonFile();
+  afx_msg void OnBnClickedButtonView();
+  afx_msg void OnBnClickedOk();
 };
